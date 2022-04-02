8b13ec2a8e8d7d17bf38c94f45bee8b251c1c719
Magomed <magomed@magomed.com>
2022-04-02 18:51:47 +0300

    udemy1

--- lesson-1.js
+++ lesson-1.js
@@ -1,2 +1,19 @@
 "use strict";
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm = prompt('Один из последних просмотренных фильмов?');
+const rateFilm = prompt('На сколько оцените его?');
+const lastFilm1 = prompt('Один из последних просмотренных фильмов?');
+const rateFilm1 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[lastFilm] = rateFilm;
+personalMovieDB.movies[lastFilm1] = rateFilm1;
+console.log(personalMovieDB);
